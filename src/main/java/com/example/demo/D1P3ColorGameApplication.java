40e5f15580276f454fe51b724531cbd9a8d34ecc
HarithKavish <harithkavish40@gmail.com>
2023-02-22 00:30:20 +0530

    commit

---
+++ src/main/java/com/example/demo/D1P3ColorGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D1P3ColorGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D1P3ColorGameApplication.class, args);
+	}
+
+}
